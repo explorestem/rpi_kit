cd407c1bf6eea85d1bad70aa28dae3dca3106ba7
David Whale <david@thinkingbinaries.com>
2016-02-14 18:43:53 +0000

    Added more setup steps

--- manual/setup.sh
+++ manual/setup.sh
@@ -2,3 +2,12 @@
 
 sudo cp cmdline.txt /boot
 sudo cp config.txt /boot
+pushd ~
+git clone https://github.com/whaleygeek/pyenergenie
+git clone https://github.com/whaleygeek/school_mc_demo
+git clone https://github.com/whaleygeek/mc_bigbang
+git clone https://github.com/whaleygeek/pibakeoff
+git clone https://github.com/whaleygeek/guessinggame
+git clone https://github.com/whaleygeek/pyws
+git clone https://github.com/whaleygeek/pyparsers
+
